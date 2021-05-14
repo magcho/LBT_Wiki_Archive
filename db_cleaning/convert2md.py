6272cbd6fc2a4ee0c529eaa79d0ee5e526100e32
magcho <mail@magcho.com>
2021-05-14 22:34:00 +0900

    Update format

--- db_cleaning/convert2md.py
+++ db_cleaning/convert2md.py
@@ -1,18 +1,24 @@
 import json
-from  pprint import  pprint
+from pprint import pprint
 
 
-def reaf_index_file():
+def read_index_file():
     entry_list = []
-    with open('./dataset/all_entry_list.json') as fp:
+    with open("./dataset/all_entry_list.json") as fp:
         all_entry_list = json.load(fp)
-        pprint(all_entry_list)
-
-        # entry_list = list(map(lambda entry: {'id': entry['id'], category: entory['category']},all_entry_list))
-        entry_list = list(map(lambda entry: {'id':entry['id'], 'title': entry['title'], 'category': entry['category'][0], 'version': entry['last-ver']}, all_entry_list))
+        entry_list = list(
+            map(
+                lambda entry: {
+                    "id": entry["id"],
+                    "title": entry["title"],
+                    "category": entry["category"][0],
+                    "version": entry["last-ver"],
+                },
+                all_entry_list,
+            )
+        )
     return entry_list
 
 
-
-if __name__ == '__main__':
-    reaf_index_file()
+if __name__ == "__main__":
+    read_index_file()
